bd9ccd0bfd7f1a03ab331e4dc9602e0b2eaccb89
Subhrajit Saha <151936320+subhoxsaha@users.noreply.github.com>
2026-04-20 04:13:50 +0530

    Update ContactSection.tsx

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,1 +1,1 @@
-<a href="https://www.linkedin.com/in/subhrajit-saha-853b293a1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">linkedin.com/in/subhrajit-saha-853b293a1</a>
+<a href="https://www.linkedin.com/in/subhrajit-saha-853b293a1">linkedin.com/in/subhrajit-saha</a>
